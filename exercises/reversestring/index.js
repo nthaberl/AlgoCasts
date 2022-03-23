45d2d72fbe454ba62e0e3a99dba6bf2e73f9ea8c
Natascha Haberl <nt.haberl@gmail.com>
2022-03-22 18:03:50 -0700

    updated

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,37 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/* function reverse(str) {
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--){
+        reversed+=str[i];
+    }
+    return reversed
+}  */
+
+//can also use the reverse method!!
+
+/* function reverse(str) {
+    return str.split("").reverse().join("");
+} */
+
+/* function reverse(str) {
+    let reversed = "";
+    for (let character of str) {
+        reversed = character + reversed;
+    }
+
+    return reversed
+} */
+
+function reverse(str) {
+    return str.split("").reduce((reversed, character) => character + reversed, '');
+}
+
+reverse("acbd")
 
 module.exports = reverse;
