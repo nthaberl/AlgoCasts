4ebe2df9a1cc9c23c78f03a422c635f6898e2a20
Natascha Haberl <nt.haberl@gmail.com>
2022-04-05 14:53:03 -0700

    updated with code

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,49 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+
+function anagrams(stringA, stringB){
+    return cleanString(stringA) === cleanString(stringB)
+}
+
+function cleanString(str){
+    //replace any characters we don't need from the string, set it all to lowercase, then split it into an array, sort, the array, then join it back together as a string!
+    return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
+
+
+/* function anagrams(stringA, stringB) {
+
+    //build a character map for each string
+    const aCharMap = buildMap(stringA);
+    const bCharMap = buildMap(stringB);
+
+    //compare how many keys each map has, if unequal, return false
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+        return false;
+    }
+
+    //iterate over the maps and compare the values!
+    for (let char in aCharMap){
+        //if the values don't match (in this case, numbers) return false
+        if(aCharMap[char] !== bCharMap[char]){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+//build a helper function that builds a character map for each string so we can later compare them
+function buildMap(str){
+    const charMap={};
+    
+    for (let char of str.replace(/[^\w]/g, "").toLowerCase()){
+        charMap[char] = charMap[char] + 1 || 1; //adds one to the character key of the map, otherwise creates a new key and gives it a value of 1
+    }
+
+    return charMap;
+} */
+
 
 module.exports = anagrams;
